13f2f485433f2bf80ac472db08082a32d0a1ed7e
Jessica-Ward03 <jessicaward.jw03@gmail.com>
2024-02-28 00:02:37 -0500

    Added snackmapvideo

--- src/fluttersnackmap/lib/main.dart
+++ src/fluttersnackmap/lib/main.dart
@@ -10,87 +10,20 @@ void main() {
 }
 
 
-
-
-//Adding a flat button
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlatButton',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
- 
-class _MyHomePageState extends State<MyHomePage> {
-  String txt='';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      //Sets up the app bar
-      appBar: AppBar(
-        title: Text('SnackMap'),
-        backgroundColor: Colors.blue,
-      ),
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-
-              //Flat button1 and what it does when pressed
-              child: Text('Flat Button1',),
-              onPressed: () {
-                setState(() {
-                  txt='FlatButton tapped1';
-                });
-              },
-            ),
-           
-           //Used for space between buttons
-            SizedBox(
-                //Use of SizedBox
-                height: 30,
-            ),
-              
-            ElevatedButton(
-
-              //Flat button2 and what it does when pressed
-              child: Text('Flat Button2',),
-              onPressed: () {
-                setState(() {
-                  txt='FlatButton tapped2';
-                });
-              },
-            ),
-
-            //Used for space between buttons
-            SizedBox(
-                //Use of SizedBox
-                height: 30,
-            ),
-            
-            ElevatedButton(
-
-              //Flat button3 and what it does when pressed
-              child: Text('Flat Button3',),
-              onPressed: () {
-                setState(() {
-                  txt='FlatButton tapped3';
-                });
-              },
-            ),
-          ],
+      title: 'SnackMap',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Center(
+            child: Text('SnackMap'),
+          ),
+        ),
+        body: Center(
+          child: Text('Hello World!'),
         ),
       ),
     );
