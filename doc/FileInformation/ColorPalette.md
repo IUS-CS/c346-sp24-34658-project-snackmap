ba30d9ad126d77571e13e2abb9490352c6da9324
Jessica-Ward03 <jessicaward.jw03@gmail.com>
2024-02-28 20:52:56 -0500

    Added a color palette, video to be used on start up, changed the homescreen and added a sidemenu.

---
+++ doc/FileInformation/ColorPalette.md
@@ -0,0 +1,19 @@
+# Color Palette
+
+
+![Thing](/doc/images/ColorPaletteImage.png?raw=true)
+
+
+To use color 1 color Color(0xff3A95D1)
+
+To use color 2 color Color(0xff2E3192)
+
+To use color 3 color Color(0xff64C19D)
+
+To use color 4 color Color(0xff7F3621)
+
+To use color 5 color Color(0xffBDBEC0)
+
+To use color 6 color Color(0xffFFFBFE)
+
+To use color 7 color Color(0xff010101)
--- src/fluttersnackmap/lib/main.dart
+++ src/fluttersnackmap/lib/main.dart
@@ -1,31 +1,128 @@
 import 'package:flutter/material.dart';
+// //import 'package:video_player/video_player.dart';
 import 'package:flutter/src/widgets/basic.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
- 
+void main() => runApp(const MyApp());
 
-//Start of app
-void main() {
-  runApp(MyApp());
-}
 
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const appTitle = 'SnackMap';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SnackMap',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Center(
-            child: Text('SnackMap'),
-          ),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Puts SnackMap in title 
+      appBar: AppBar(
+        backgroundColor: Color(0xff3A95D1),
+        title: Center(
+          child: Text('SnackMap'),
+        ),
+      ),
+      //Displays the text
+      body: Center(
+        child: Text(
+          'Will later add SnackMapLogo.mp4 Here for animation'
+        ),
+      ),
+      drawer: Drawer(
+        child: 
+        ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+              color: Color(0xff64C19D),
+            ),
+            child: Text(
+              'Find A Snack By',
+              style: TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ListTile(
+          leading: Icon(Icons.manage_search),
+          title:const Text(
+              'Name',
+              style: TextStyle(
+              fontSize: 15.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          subtitle:const Text('Search for a drink or snack by name.'),
+          //tileColor: Color(0xff64C19D),
+          onTap:(){
+            //Will later update
+            //Closes the drawer
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.room),
+          title: const Text(
+            'Location',
+              style: TextStyle(
+              fontSize: 15.0,
+              fontWeight: FontWeight.w600,
+              ),
+            ),
+          subtitle:const Text('Search for a drink or snack by location.'),
+          //tileColor: Color(0xff7F3621),
+          onTap:(){
+            //Will later update.
+            //Closes the drawer
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.paid),
+          title: const Text(
+            'Price',
+              style: TextStyle(
+              fontSize: 15.0,
+              fontWeight: FontWeight.w600,
+              )
+            ),
+          subtitle:const Text('Search for a drink or snack by price.'),
+          //tileColor: Color(0xffBDBEC0),
+          onTap: (){
+            //Will later update.
+            //Closes the drawer
+            Navigator.pop(context);
+           },
+         ),
+       ],
+      ),
+    ),
+  );
+ }
+}
+
+
